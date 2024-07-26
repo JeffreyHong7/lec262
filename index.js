2389507b8ef0d51a78b45943d075a8efca6f7d87
Jeff <jmh533@cornell.edu>
2024-07-25 21:10:23 -0400

    complete activity

--- index.js
+++ index.js
@@ -1,34 +1,81 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
 
+// initialize import variables
 const app = express();
 const port = 3000;
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public"));
-
+const client = new pg.Client({
+  user: "postgres",
+  password: "Jalapeno.70",
+  host: "localhost",
+  port: 5432,
+  database: "permalist",
+});
 let items = [
   { id: 1, title: "Buy milk" },
   { id: 2, title: "Finish homework" },
 ];
 
-app.get("/", (req, res) => {
+// initialize helpers
+client.connect();
+async function getItems() {
+  try {
+    const result = await client.query("SELECT * FROM items ORDER BY id ASC");
+    items = result.rows;
+  } catch (err) {
+    console.error("Failed to get items", err.stack);
+  }
+}
+
+// initialize middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+// initialize route-handlers
+app.get("/", async (req, res) => {
+  await getItems();
   res.render("index.ejs", {
     listTitle: "Today",
     listItems: items,
   });
 });
 
-app.post("/add", (req, res) => {
-  const item = req.body.newItem;
-  items.push({ title: item });
-  res.redirect("/");
+app.post("/add", async (req, res) => {
+  try {
+    const item = req.body.newItem;
+    await client.query("INSERT INTO items (title) VALUES ($1)", [item]);
+    res.redirect("/");
+  } catch (err) {
+    console.error("Failed to add item", err.stack);
+  }
 });
 
-app.post("/edit", (req, res) => {});
+app.post("/edit", async (req, res) => {
+  try {
+    const id = req.body.updatedItemId;
+    const newTitle = req.body.updatedItemTitle;
+    await client.query("UPDATE items SET title = $2 WHERE id = $1", [
+      id,
+      newTitle,
+    ]);
+    res.redirect("/");
+  } catch (err) {
+    console.error("Failed to update item".err.stack);
+  }
+});
 
-app.post("/delete", (req, res) => {});
+app.post("/delete", async (req, res) => {
+  try {
+    const id = req.body.deleteItemId;
+    await client.query("DELETE FROM items WHERE id = $1", [id]);
+    res.redirect("/");
+  } catch (err) {
+    console.error("Failed to delete item", err.stack);
+  }
+});
 
+// start server
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
--- solution.js
+++ solution.js
@@ -26,7 +26,6 @@ app.get("/", async (req, res) => {
   try {
     const result = await db.query("SELECT * FROM items ORDER BY id ASC");
     items = result.rows;
-
     res.render("index.ejs", {
       listTitle: "Today",
       listItems: items,
